f2f958acf84cd87a11366bd6e6784bca6142b346
Isaac Yauk <isaacyauk@yahoo.com>
2023-04-03 16:46:50 -0600

    Code Creation; writes constructors and adds some code for Lab9

--- csci132_Lab9/src/Car.java
+++ csci132_Lab9/src/Car.java
@@ -1,4 +1,18 @@
 public class Car
 {
+    // make & model
+    // owner name
+    // VIP member holder
+    private String makeAndModel;
+    private String ownerName;
+    private boolean VIPStatus;
+
+    public Car (String mdl, String name, Boolean isVIP)
+    {
+        this.makeAndModel = mdl;
+        this.ownerName = name;
+        this.VIPStatus = isVIP;
+    }
+
 
 }
--- csci132_Lab9/src/QueueArray.java
+++ csci132_Lab9/src/QueueArray.java
@@ -1,4 +1,33 @@
 public class QueueArray
 {
+    private Car[] car_line;
+    private int size;
+    private int front;
+    private int back;
+    private int capacity;
+
+    public QueueArray(int size)
+    {
+        this.car_line = new car_line[size];
+        this.size = 0;
+        this.front = 0;
+        this.back = 0;
+        this.capacity = this.car_line.length;
+    }
+
+    public void enqueue(Car newCar)
+    {
+        if(this.capacity == 0)
+        {
+            System.out.println("Error; Queue is full!");
+            return;
+        }
+        else
+        {
+            this.car_line[this.back] = newCar;
+            this.back++;
+            this.size++;
+        }
+    }
 
 }
