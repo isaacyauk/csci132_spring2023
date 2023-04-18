9a3e1429fdf216bf6b6393e7586c5d0ce5cabc1d
Isaac Yauk <isaacyauk@yahoo.com>
2023-04-18 17:24:51 -0600

    Finishes Lab solution

--- MiscNotes/csci132_Lab11/src/Lab11Demo.java
+++ MiscNotes/csci132_Lab11/src/Lab11Demo.java
@@ -4,17 +4,59 @@ public class Lab11Demo
 {
     public static String fix_string(String s)
     {
-        //TO DO: Use recursion to solve this problem
+        //TODO: Use recursion to solve this problem
+        if(s.length() == 0)
+        {
+            return "";
+        }
+        else
+        {
+            if(s.charAt(0) == 'g')
+            {
+                return "c" + fix_string(s.substring(1));
+            }
+            if(s.charAt(0) == 'a')
+            {
+                return "t" + fix_string(s.substring(1));
+            }
+            if(s.charAt(0) == 't')
+            {
+                return "a" + fix_string(s.substring(1));
+            }
+            if(s.charAt(0) == 'c')
+            {
+                return "g" + fix_string(s.substring(1));
+            }
 
 
+            else
+            {
+                return "";
+            }
+        }
     }
 
-    public static void print_stack_reverse_recursive(Stack<String> stack)
+    public static void print_stack_reverse_recursive(Stack<String> incoming_stack)
     {
-        //TO DO: Use recursion to solve this problem
+        if(incoming_stack.size() == 0)
+        {
+            return;
+        }
+        else
+        {
+            String removed = incoming_stack.pop();
+            print_stack_reverse_recursive(incoming_stack);
 
+            System.out.println("Original String:");
+            System.out.println(removed);
+            System.out.println("Fixed String:");
+
+            String corrected_string = fix_string(removed);
+            System.out.println(corrected_string);
+            System.out.println("-----------");
+        }
     }
-    
+
     //You should NOT change anything below this comment
     public static void main(String[] args)
     {
