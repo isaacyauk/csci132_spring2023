154f9e1e9d441011b7d22bb83c96a132b918bc6d
Isaac Yauk <isaacyauk@yahoo.com>
2023-04-27 12:45:18 -0600

    Implements 2nd recursive method for Program 5 solution

--- csci132_Program5/src/Program5Demo.java
+++ csci132_Program5/src/Program5Demo.java
@@ -21,34 +21,34 @@ public class Program5Demo {
         answer = is_palindrome(test);
         System.out.println("Word: " + test + ", Is palindrome: " + answer);
 
-//        System.out.println("-----------------------------------------------------");
-//
-//        System.out.println("Part II: Binary Search");
-//        int[] array = {2, 6, 12, 14, 17, 19, 20, 23, 27, 29, 32, 37, 40, 42, 45};
-//        int num, answer2;
-//
-//        int low = 0;
-//        int high = array.length - 1;
-//
-//        System.out.println(Arrays.toString(array));
-//
-//        num = 12;
-//        answer2 = binary_search(array,num,low,high);
-//        System.out.println(num + " is located at index: " + answer2);
-//
-//        num = 45;
-//        answer2 = binary_search(array,num,low,high);
-//        System.out.println(num + " is located at index: " + answer2);
-//
-//        num = 29;
-//        answer2 = binary_search(array,num,low,high);
-//        System.out.println(num + " is located at index: " + answer2);
-//
-//        num = 55;
-//        answer2 = binary_search(array,num,low,high);
-//        System.out.println(num + " is located at index: " + answer2);
-//
-//        System.out.println("-----------------------------------------------------");
+        System.out.println("-----------------------------------------------------");
+
+        System.out.println("Part II: Binary Search");
+        int[] array = {2, 6, 12, 14, 17, 19, 20, 23, 27, 29, 32, 37, 40, 42, 45};
+        int num, answer2;
+
+        int low = 0;
+        int high = array.length - 1;
+
+        System.out.println(Arrays.toString(array));
+
+        num = 12;
+        answer2 = binary_search(array,num,low,high);
+        System.out.println(num + " is located at index: " + answer2);
+
+        num = 45;
+        answer2 = binary_search(array,num,low,high);
+        System.out.println(num + " is located at index: " + answer2);
+
+        num = 29;
+        answer2 = binary_search(array,num,low,high);
+        System.out.println(num + " is located at index: " + answer2);
+
+        num = 55;
+        answer2 = binary_search(array,num,low,high);
+        System.out.println(num + " is located at index: " + answer2);
+
+        System.out.println("-----------------------------------------------------");
 //        System.out.println("Part III and IV: Finding minimum number of coins");
 //        int k; //target value
 //        int n; // answer for part III
@@ -95,9 +95,34 @@ public class Program5Demo {
 
     private static int binary_search(int[] array, int num, int low, int high) {
 
-        //TO DO: Part I: Fill in method using recursion, or have it call another method that uses recursion
+        if (low <= high)
+        {
+            // Divide the stuff in half and use that number to start looking through either side of the array
+            int mid = (low + high ) / 2;
 
-        return -1;
+            // The case where you find the exact number you're looking for, based on the number you generated above
+            if (num == array[mid])
+            {
+                return mid;
+            }
+
+            // The case where you look through your array again, if the number you're looking for is greater than the number you generated above
+            else if(num > array[mid])
+            {
+                // Adjust the low parameters here, so you don't recurse infinitely
+                return binary_search(array, num, mid + 1, high);
+            }
+            else
+            {
+                // Adjust the high parameters here, so you don't recurse infinitely
+                return binary_search(array, num, low, mid - 1);
+            }
+        }
+        else
+        {
+            // Number was not in the array
+            return -1;
+        }
     }
 
 
