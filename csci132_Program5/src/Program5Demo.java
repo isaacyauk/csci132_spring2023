5802e0313b4ccf5ea5dfb2de77ecce3c7426fbec
Isaac Yauk <isaacyauk@yahoo.com>
2023-04-27 11:55:44 -0600

    Program5 update; completes 1st required method

--- csci132_Program5/src/Program5Demo.java
+++ csci132_Program5/src/Program5Demo.java
@@ -21,64 +21,76 @@ public class Program5Demo {
         answer = is_palindrome(test);
         System.out.println("Word: " + test + ", Is palindrome: " + answer);
 
-        System.out.println("-----------------------------------------------------");
-
-        System.out.println("Part II: Binary Search");
-        int[] array = {2, 6, 12, 14, 17, 19, 20, 23, 27, 29, 32, 37, 40, 42, 45};
-        int num, answer2;
-
-        int low = 0;
-        int high = array.length - 1;
-
-        System.out.println(Arrays.toString(array));
-
-        num = 12;
-        answer2 = binary_search(array,num,low,high);
-        System.out.println(num + " is located at index: " + answer2);
-
-        num = 45;
-        answer2 = binary_search(array,num,low,high);
-        System.out.println(num + " is located at index: " + answer2);
-
-        num = 29;
-        answer2 = binary_search(array,num,low,high);
-        System.out.println(num + " is located at index: " + answer2);
-
-        num = 55;
-        answer2 = binary_search(array,num,low,high);
-        System.out.println(num + " is located at index: " + answer2);
-
-        System.out.println("-----------------------------------------------------");
-        System.out.println("Part III and IV: Finding minimum number of coins");
-        int k; //target value
-        int n; // answer for part III
-        int[] D = {1, 5, 10, 18, 25};
-
-        k = 37; //find minimum number of coins from D to make value K
-        n = min_coins(D, k);
-        System.out.println("Minimum coins needed to create " + k + ": " + n);
-        System.out.println("Coins used:");
-        find_coins(D,k,n);
-
-        k = 4; //find minimum number of coins from D to make value K
-        n = min_coins(D, k);
-        System.out.println("Minimum coins needed to create " + k + ": " + n);
-        System.out.println("Coins used:");
-        find_coins(D,k,n);
-
-        k = 81; //find minimum number of coins from D to make value K
-        n = min_coins(D, k);
-        System.out.println("Minimum coins needed to create " + k + ": " + n);
-        System.out.println("Coins used:");
-        find_coins(D,k,n);
+//        System.out.println("-----------------------------------------------------");
+//
+//        System.out.println("Part II: Binary Search");
+//        int[] array = {2, 6, 12, 14, 17, 19, 20, 23, 27, 29, 32, 37, 40, 42, 45};
+//        int num, answer2;
+//
+//        int low = 0;
+//        int high = array.length - 1;
+//
+//        System.out.println(Arrays.toString(array));
+//
+//        num = 12;
+//        answer2 = binary_search(array,num,low,high);
+//        System.out.println(num + " is located at index: " + answer2);
+//
+//        num = 45;
+//        answer2 = binary_search(array,num,low,high);
+//        System.out.println(num + " is located at index: " + answer2);
+//
+//        num = 29;
+//        answer2 = binary_search(array,num,low,high);
+//        System.out.println(num + " is located at index: " + answer2);
+//
+//        num = 55;
+//        answer2 = binary_search(array,num,low,high);
+//        System.out.println(num + " is located at index: " + answer2);
+//
+//        System.out.println("-----------------------------------------------------");
+//        System.out.println("Part III and IV: Finding minimum number of coins");
+//        int k; //target value
+//        int n; // answer for part III
+//        int[] D = {1, 5, 10, 18, 25};
+//
+//        k = 37; //find minimum number of coins from D to make value K
+//        n = min_coins(D, k);
+//        System.out.println("Minimum coins needed to create " + k + ": " + n);
+//        System.out.println("Coins used:");
+//        find_coins(D,k,n);
+//
+//        k = 4; //find minimum number of coins from D to make value K
+//        n = min_coins(D, k);
+//        System.out.println("Minimum coins needed to create " + k + ": " + n);
+//        System.out.println("Coins used:");
+//        find_coins(D,k,n);
+//
+//        k = 81; //find minimum number of coins from D to make value K
+//        n = min_coins(D, k);
+//        System.out.println("Minimum coins needed to create " + k + ": " + n);
+//        System.out.println("Coins used:");
+//        find_coins(D,k,n);
 
     }
 
-    private static boolean is_palindrome(String test) {
+    private static boolean is_palindrome(String str)
+    {
+        if (str.length() <= 1)
+        {
+            // An empty string is technically a palindrome.
+            return true;
+        }
+        else if (str.charAt(0) == str.charAt(str.length() - 1))
+        {
+            // Recursive case where the first and last char are checked to see if they're matching; a non-palindrome will not pass this check
+            return is_palindrome(str.substring(1, str.length() - 1));
+        }
+        else
+        {
+            return false;
+        }
 
-        //TO DO: Part I: Fill in method using recursion, or have it call another method that uses recursion
-
-        return false;
     }
 
     private static int binary_search(int[] array, int num, int low, int high) {
